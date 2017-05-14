6af9bfb97e525cff86e3d9f1f6ccd99df4819ea8
freund17 <lukas.aymans@googlemail.com>
2017-05-14 22:01:48 +0200

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # axios-cloudflare
-An axios interceptor to handle cloudflare browserchecks
+
+An axios hook to handle cloudflare ddos protection / browsercheck
+
+## Usage
+
+```[JavaScript]
+const axios = require('axios');
+const axiosCloudflare = require('axios-cloudflare');
+
+axiosCloudflare(axios);
+
+axios.get('[Your Url]').then(response => {
+    console.log(response.data);
+}).catch(error => {
+    console.error(error);
+});
+```
---
+++ index.js
@@ -0,0 +1,115 @@
+const cheerio = require('cheerio');
+const delay = require('delay');
+const url = require('url');
+const tough = require('tough-cookie');
+
+const clearenceCookies = {};
+
+function axiosCloudflare(axios) {
+    function isCloudflareResponse(response) {
+        if(typeof response.headers['cf-ray'] !== 'undefined' && typeof response.headers['server'] !== 'undefined' && response.headers['server'] === 'cloudflare-nginx') {
+            response.$ = cheerio.load(response.data);
+
+            if(response.$('#challenge-form input[name=jschl_vc]').length > 0)
+                return true;
+        }
+
+        return false;
+    }
+
+    function interceptCloudflare(response) {
+        const $ = response.$;
+
+        const cookie__cfduid = tough.Cookie.parse(response.headers['set-cookie'][0]);
+
+        const $form = $('#challenge-form');
+        const targetUrl = $form.attr('action');
+        const jschl_vc = $form.find('input[name=jschl_vc]').attr('value');
+        const pass = $form.find('input[name=pass]').attr('value');
+
+        const urlObj = url.parse(response.config.url);
+        const scriptRows = $('script').first().html().split('\n');
+
+        const part1 = scriptRows[8].match(/^.*, (\w+)={"(\w+)":([!+()[\]]+)};.*$/);
+
+        if(part1 === null)
+            throw new Error('Cloudflare interception failed!');
+
+        const varname = part1[1] + '.' + part1[2];
+        let jschl_answer = eval(part1[3]);
+
+        const challanges = scriptRows[15].split(';');
+
+        {
+            let x = jschl_answer;
+            const saveToExec = /^x[+\-*]=[!+()[\]]+$/;
+
+            challanges.forEach(challenge => {
+                challenge = challenge.replace(varname, 'x');
+
+                if(saveToExec.test(challenge))
+                    eval(challenge);
+            });
+
+            jschl_answer = x;
+        }
+
+        jschl_answer += urlObj.hostname.length;
+
+        return delay(4000).then(() => {
+            return axios({
+                method: 'get',
+                url: url.resolve(urlObj.href, targetUrl),
+                params: { jschl_vc, pass, jschl_answer },
+                headers: {
+                    'Referer': response.config.url
+                },
+                validateStatus: status => status === 302,
+                maxRedirects: 0
+            }).then(response => {
+                response.headers['set-cookie'].forEach(cookieString => {
+                    const cookie = tough.Cookie.parse(cookieString);
+
+                    if(cookie.key === 'cf_clearance')
+                        clearenceCookies[urlObj.hostname] = cookie.value;
+                });
+
+                return axios.get(response.headers['location']);
+            });
+        });
+    }
+
+    function cloudflareRequestInterceptor(config) {
+        const urlObj = url.parse(config.url);
+
+        if(typeof clearenceCookies[urlObj.hostname] !== 'undefined') {
+            if(typeof config.headers['cookie'] === 'undefined')
+                config.headers['cookie'] = '';
+            else
+                config.headers['cookie'] += ';';
+
+            config.headers['cookie'] += 'cf_clearance=' + clearenceCookies[urlObj.hostname];
+        }
+
+        return config;
+    }
+
+    function cloudflareResponseInterceptor(response) {
+        if(isCloudflareResponse(response))
+            return interceptCloudflare(response);
+        
+        return response;
+    }
+
+    function cloudflareResponseErrorInterceptor(error) {
+        if(isCloudflareResponse(error.response))
+            return interceptCloudflare(error.response);
+        
+        return Promise.reject(error);
+    }
+
+    axios.interceptors.request.use(cloudflareRequestInterceptor);
+    axios.interceptors.response.use(cloudflareResponseInterceptor, cloudflareResponseErrorInterceptor);
+}
+
+module.exports = axiosCloudflare;
